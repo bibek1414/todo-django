231df480a54640ac9eedc29e0937af96792846d2
bibek <bbhattarai770@gmail.com>
2025-02-08 13:21:03 +0545

    Static files changed

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -124,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Add this line to your settings.py
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
